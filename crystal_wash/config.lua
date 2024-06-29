2eabb95db8df2c60a2d49991408392a6b498020b
Crystal-Script <158371721+Crystal-Script@users.noreply.github.com>
2024-06-29 16:06:33 +0200

    Update config.lua

--- crystal_wash/config.lua
+++ crystal_wash/config.lua
@@ -1,30 +1,18 @@
 Config = {}
 
-Config.robberies = {
-    ['jewelry'] = {
-        label = "Negozio di Gioielli",
-        position = { x = -631.43, y = -236.13, z = 38.05 },
-        blipSprite = 617,
-        blipColor = 47,
-        blipScale = 0.8,
-        blipText = "Rapina: Negozio di Gioielli",
-        maxDistance = 10.0, -- Distanza massima per attivare la rapina (in metri)
-        cooldown = 600, -- Tempo di attesa tra una rapina e l'altra (in secondi)
-        requiredItems = {
-            {name = "lockpick", label = "Grimaldello", count = 1}
-        }
+Config.wash = {
+    ['wash1'] = {
+        position = vector3(-700, -932.3396, 19.0139),
+        blip = 100,
+        blipColor = 0,
+        blipScale = 1.0,
+        blipText = "Lavaggio veicoli",
     },
-    ['bank'] = {
-        label = "Banca Centrale",
-        position = { x = 150.0, y = -1040.0, z = 29.4 },
-        blipSprite = 108,
-        blipColor = 2,
-        blipScale = 0.8,
-        blipText = "Rapina: Banca Centrale",
-        maxDistance = 15.0,
-        cooldown = 900,
-        requiredItems = {
-            {name = "thermite", label = "Termite", count = 1}
-        }
+    ['wash2'] = {
+        position = vector3(22.3182, -1392.0071, 29.3292),
+        blip = 100,
+        blipColor = 0,
+        blipScale = 1.0,
+        blipText = "Lavaggio veicoli",
     }
 }
